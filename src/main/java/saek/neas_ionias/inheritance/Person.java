b522693fd1e7cbc35182729d638800c80ec3a3e9
LAB-A-PC5 <LAB-A-PC5@LAB-A-PC5.lan>
2025-01-07 19:52:36 +0200

    created Person Class

---
+++ src/main/java/saek/neas_ionias/inheritance/Person.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package saek.neas_ionias.inheritance;
+
+/**
+ *
+ * @author LAB-A-PC5
+ */
+public class Person {
+    
+    private String name;
+    
+    private int age;
+    
+    private int id;
+
+    /**
+     * Get the value of id
+     *
+     * @return the value of id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @param id new value of id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    /**
+     * Get the value of age
+     *
+     * @return the value of age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * Set the value of age
+     *
+     * @param age new value of age
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    /**
+     * Get the value of name
+     *
+     * @return the value of name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @param name new value of name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
