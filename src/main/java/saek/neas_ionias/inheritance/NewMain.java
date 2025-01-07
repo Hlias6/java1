5c3c7e12712fbe36bf2b2a490c1ec8c6553b22dd
LAB-A-PC5 <LAB-A-PC5@LAB-A-PC5.lan>
2025-01-07 19:42:10 +0200

    created Main Class

---
+++ src/main/java/saek/neas_ionias/inheritance/NewMain.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package saek.neas_ionias.inheritance;
+
+/**
+ *
+ * @author LAB-A-PC5
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
